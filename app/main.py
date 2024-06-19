5165187a363407b551e80e82eac4a2c7efe147c3
piotr-druzdzel <piotr.druzdzel@gmail.com>
2024-06-19 12:36:25 +0200

    Adding PostgreSQL - error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
